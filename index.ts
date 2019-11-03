206619147a642a5c7c81006c4ca5676e55a81694
Sebastien Braun <sebastien@sebbraun.de>
2019-11-03 14:19:27 +0100

    Add license header

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+// Copyright (c) 2019 Sebastien Braun
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import { Compiler, Stats } from "webpack";
 import { fromHex as sriFromHex } from "ssri";
 import { readFileSync, statSync, writeFileSync } from "fs";
